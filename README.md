bd03cc2dfe8a07aa9c5878217e16f8f71c906bc2
BritishPandaChick <sbartley1080@gmail.com>
2022-03-11 02:20:35 -0500

    Add code files

---
+++ README.md
@@ -0,0 +1,9 @@
+# Advent of JavaScript Challenge
+All projects from the Advent of CSS challenge.
+
+## Contributons
+I will not be accepting any pull requests.
+
+## Acknowledgements
+* Amy Dutton
+* James Q Quick
---
+++ day-one/01__POMODORO/STARTER-FILES/script.js
@@ -0,0 +1,5 @@
+//Starts the timer 
+//User clicks start, word start changes to start 
+//User can click the stop button to make time stop 
+//Timer finishes, ring changes from green to red
+//Alert message is passed to the browser
